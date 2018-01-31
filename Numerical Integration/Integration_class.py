4727c5e6e363ec7cf3aa87155c6b4f80a7570f99
ElNinoGit9 <quakeCaptain@hotmail.com>
2018-01-31 23:13:26 +0100

    Modified: Numerical Integration

--- Numerical Integration/Integration_class.py
+++ Numerical Integration/Integration_class.py
@@ -8,7 +8,6 @@ class IntegrationClass:
         self.func = f
         self.dx = (maxv-minv)/float(N)
 
-
     def integrate(self):
 
         if self.method is 'Midpoint':
@@ -19,6 +18,10 @@ class IntegrationClass:
             self.SimpsonsRule()
         elif self.method is 'Boole':
             self.BoolesRule()
+        elif self.method is 'Legendre':
+            self.GaussLegendre()
+        elif self.method is 'Lobatto':
+            self.GaussLobatto()
 
     def MidpointRule(self):
         import numpy as np
@@ -54,8 +57,60 @@ class IntegrationClass:
         + 12*np.sum(self.func(self.grid[2:-1:4])) + 32*np.sum(self.func(self.grid[3:-1:4])) \
         + 14*np.sum(self.func(self.grid[4:-1:4])) + 7*self.func(self.grid[-1]))
 
+    def GaussLegendre(self):
+        import numpy as np
+        print 'Gauss-Legendre'
+
+        if self.N == 1:
+            p = np.array([0])
+            w = np.array([2])
+        elif self.N == 2:
+            p = np.array([0.57735, -.57735])
+            w = np.array([1, 1])
+        elif self.N == 3:
+            p = np.array([0, 0.774597, -0.774597])
+            w = np.array([0.888889, 0.555556, 0.555556])
+        elif self.N == 4:
+            p = np.array([0.339981, -0.339981, 0.861136, -0.861136])
+            w = np.array([0.652145, 0.652145, 0.347855, 0.347855])
+        elif self.N == 5:
+            p = np.array([0, 0.538469, -0.538469, 0.90618, -0.90618])
+            w = np.array([0.568889, 0.478629, 0.478629, 0.236927, 0.236927])
+        else:
+            print 'number of points undefined'
+
+        a = (self.maxv - self.minv)/2.
+        b = (self.maxv + self.minv)/2.
+        self.I = a * np.dot(self.func(a*p + b), w)
+
+    def GaussLobatto(self):
+        import numpy as np
+        print 'Gauss-Lobatto'
+
+        if self.N == 3:
+            p = np.array([0, 1, -1])
+            w = np.array([4/3., 1/3., 1/3.])
+        elif self.N == 4:
+            p = np.array([np.sqrt(1/5.), -np.sqrt(1/5.), 1, -1])
+            w = np.array([5/6., 5/6., 1/6., 1/6.])
+        elif self.N == 5:
+            p = np.array([0, np.sqrt(3./7.), -np.sqrt(3./7.), 1, -1])
+            w = np.array([32/45., 49/90., 49/90., 1/10., 1/10.])
+        elif self.N == 6:
+            p = np.array([np.sqrt(1/3. - 2*np.sqrt(7)/21.), -np.sqrt(1/3. - 2*np.sqrt(7)/21.), np.sqrt(1/3. + 2*np.sqrt(7)/21.), -np.sqrt(1/3. + 2*np.sqrt(7)/21.), 1, -1])
+            w = np.array([(14 + np.sqrt(7))/30., (14 + np.sqrt(7))/30., (14 - np.sqrt(7))/30., (14 - np.sqrt(7))/30., 1/15., 1/15.])
+        elif self.N == 7:
+            p = np.array([0, np.sqrt(5/11. - 2/11.*np.sqrt(5/3.)), -np.sqrt(5/11. - 2/11.*np.sqrt(5/3.)), np.sqrt(5/11. + 2/11.*np.sqrt(5/3.)), -np.sqrt(5/11. + 2/11.*np.sqrt(5/3.)), 1, -1])
+            w = np.array([256/525., (124 + 7*np.sqrt(15))/350., (124 + 7*np.sqrt(15))/350., (124 - 7*np.sqrt(15))/350., (124 - 7*np.sqrt(15))/350., 1/21., 1/21.])
+        else:
+            print 'number of points undefined'
+
+        a = (self.maxv - self.minv)/2.
+        b = (self.maxv + self.minv)/2.
+        self.I = a * np.dot(self.func(a*p + b), w)
+
 def f (x): return x*x*x*x
 
-Int = IntegrationClass(0, 1, 12, 'Boole', f)
+Int = IntegrationClass(0, 1, 5, 'Lobatto', f)
 Int.integrate()
 print Int.I
