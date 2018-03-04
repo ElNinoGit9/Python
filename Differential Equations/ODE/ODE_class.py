a4b2192d778045a562cd258bd883799ccb489d6d
ElNinoGit9 <quakeCaptain@hotmail.com>
2018-03-04 22:07:00 +0100

    Rearrangements

--- Differential Equations/ODE/ODE_class.py
+++ Differential Equations/ODE/ODE_class.py
@@ -19,30 +19,9 @@ class ODEClass:
         elif self.method is 'RungeKutta4':
             from RungeKutta4 import RungeKutta4
             [self.t, self.y] = RungeKutta4([self.minv, self.maxv], self.f0, self.funct, self.N)
-        elif self.method is 'Leapfrog':
-            from Leapfrog import Leapfrog
-            [self.t, self.y] = Leapfrog([self.minv, self.maxv], self.f0, self.funct, self.N)
-        elif self.method is 'TrapezoidalRule':
-            from TrapezoidalRule import TrapezoidalRule
-            [self.t, self.y] = TrapezoidalRule([self.minv, self.maxv], self.f0, self.funct, self.N)
-        elif self.method is 'MidpointMethod':
-            from MidpointMethod import MidpointMethod
-            [self.t, self.y] = MidpointMethod([self.minv, self.maxv], self.f0, self.funct, self.N)
-
-    def Leapfrog(self):
-        import numpy as np
-        print 'RungaKuttaFehlberg'
-
-    def TrapezoidalRule(self):
-        import numpy as np
-        print 'ABMM'
-
-    def MidpointMethod(self):
-        import numpy as np
-        print 'MilneSimpson'
 
 def ft(t, y): return y + t
 
-Ode = ODEClass(0, 1, 500, 'RungeKutta4', ft, 0)
+Ode = ODEClass(0, 1, 500, 'MidpointMethod', ft, 0)
 Ode.Solve()
 print Ode.t, Ode.y
