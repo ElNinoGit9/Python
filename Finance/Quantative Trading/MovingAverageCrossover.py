cefd8b181b07ede6deab905bdbf58d40cdcee5d9
ElNinoGit9 <quakeCaptain@hotmail.com>
2018-09-26 16:22:08 +0200

    Modified Moving average algorithm

--- Finance/Quantative Trading/MovingAverageCrossover.py
+++ Finance/Quantative Trading/MovingAverageCrossover.py
@@ -1,10 +1,13 @@
-def MovingAverageCrossover(signals, a, b, n):
+def MovingAverageCrossover(stockPrices, a, b, n, amounts):
     import numpy as np
 
-    short = np.sum(signals['Adj Close'][n-a:n])/float(a)
-    long = np.sum(signals['Adj Close'][n-b:n])/float(b)
+    short = np.sum(stockPrices[n-a:n])/a
+    long = np.sum(stockPrices[n-b:n])/b
+    bestInd = np.argmax([long, short])
 
-    if long < short:
-        return 1
-    else:
-        return -1
+    if amounts[bestInd] == 1: # If the best the current setting
+        return 0, 0 # Do nothing
+    elif bestInd == 1: # If not, and we want to go long
+        return 1, 0 # Buy
+    else: # Else, go short
+        return 0, 1 # Sell
--- Finance/Quantative Trading/QuantitativeTrading.py
+++ Finance/Quantative Trading/QuantitativeTrading.py
@@ -1,27 +1,13 @@
-class StrategyClass:
-    def __init__(self, method, stock, startDate, endDate, a, b):
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+class DataClass:
+
+    def __init__(self, stock, startDate, endDate):
         import pandas as pd
         import pandas_datareader as pdr
         import datetime
         import quandl
-        import numpy as np
-        import fix_yahoo_finance as yf
-
-        self.initStocks(stock, startDate, endDate)
-
-        if method is 'MovingAverageCrossover':
-            from MovingAverageCrossover import MovingAverageCrossover
-            self.method = MovingAverageCrossover
-            self.methodName = 'Moving Average Crossover'
-            self.a = a
-            self.b = b
-
-    def initStocks(self, stock, startDate, endDate):
-        import pandas as pd
-        import pandas_datareader as pdr
-        import datetime
-        import quandl
-        import numpy as np
         import fix_yahoo_finance as yf
 
         def get(tickers, startdate, enddate):
@@ -31,54 +17,69 @@ class StrategyClass:
           return(pd.concat(datas, keys=tickers, names=['Ticker', 'Date']))
 
         tickers = [stock]
-        self.signals = get(tickers, datetime.datetime(startDate[0], startDate[1], startDate[2]), datetime.datetime(endDate[0], endDate[1], endDate[2]))
+        self.data = get(tickers, datetime.datetime(startDate[0], startDate[1], startDate[2]), datetime.datetime(endDate[0], endDate[1], endDate[2]))
+        self.stockPrices = [n for n in self.data['Adj Close']]
 
-    def Run(self):
-        print('Run strategy')
-        import pandas as pd
-        import pandas_datareader as pdr
-        import datetime
-        import quandl
-        import numpy as np
-        import fix_yahoo_finance as yf
+class StrategyClass:
+    def __init__(self, method, a, b):
+
+        if method is 'MovingAverageCrossover':
+            from MovingAverageCrossover import MovingAverageCrossover
+            self.method = MovingAverageCrossover
+            self.methodName = 'Moving Average Crossover'
+            self.a = a
+            self.b = b
+
+    def Run(self, data_obj):
 
         solver = self.method
 
         self.money = 100
         self.share = 0
 
-        nDays = len(self.signals)
+        nDays = len(data_obj.stockPrices)
+
         self.value = np.zeros(nDays - self.b + 1)
-        self.comp = np.zeros(nDays - self.b + 1)
+        self.comp  = np.zeros(nDays - self.b + 1)
 
         self.value[0] = 100
-        self.comp[0] = 100
 
-        self.BuySell(self.signals, 1, self.b)
+        self.startDate = 200
+        amounts = [1, 0] # Start with 100% money
+        buyAmounts, sellAmounts = solver(data_obj.stockPrices, self.a, self.b, self.b, amounts)
+
+        self.Buy(buyAmounts, data_obj.stockPrices[self.startDate])
+        self.Sell(sellAmounts, data_obj.stockPrices[self.startDate])
 
         for n in range(self.b, nDays):
 
-            indBuySell = solver(self.signals, self.a, self.b, n)
-            self.BuySell(self.signals, indBuySell, n)
+            totalNetWorth = self.share * data_obj.stockPrices[n] + self.money
+            amounts = [self.money / totalNetWorth, self.share * data_obj.stockPrices[n] / totalNetWorth]
 
-            self.value[n - self.b + 1] = self.money + self.share * self.signals['Adj Close'][n]
-            self.comp[n - self.b + 1] = self.comp[0] * self.signals['Adj Close'][n]/self.signals['Adj Close'][self.b]
+            buyAmounts, sellAmounts = solver(data_obj.stockPrices, self.a, self.b, n, amounts)
 
-    def BuySell(self, signals, BuySellInd, n):
+            self.Buy(buyAmounts, data_obj.stockPrices[n])
+            self.Sell(sellAmounts, data_obj.stockPrices[n])
 
-        if BuySellInd > 0:
-            price = signals['Adj Close'][n]
-            self.share = self.share + BuySellInd * self.money/price
-            self.money = (1 - BuySellInd) * self.money
-        elif BuySellInd < 0:
-            price = signals['Adj Close'][n]
-            self.money = self.money - BuySellInd * self.share * price
-            self.share = (1 + BuySellInd) * self.share
+            self.value[n - self.b + 1] = self.money + self.share * data_obj.stockPrices[n]
 
+
+    def Buy(self, amount, price):
+
+            self.share = self.share + amount * self.money/price
+            self.money = (1 - amount) * self.money
+
+    def Sell(self, amount, price):
+
+            self.money = self.money + amount * self.share * price
+            self.share = (1 - amount) * self.share
+
+            return [amount, 1 - amount]
 
     def Plot(self):
         import matplotlib.pyplot as plt
         # Initialize the plot figure
+        self.comp  = self.value[0] * data_obj.stockPrices / data_obj.stockPrices[self.startDate]
         fig = plt.figure()
 
         plt.xlabel('t (days)')
@@ -88,7 +89,26 @@ class StrategyClass:
         plt.legend(['Moving Average', 'All in'], loc=1, frameon=False)
         plt.show()
 
-g = StrategyClass('MovingAverageCrossover', 'AAPL', [2012, 10, 1], [2018, 1, 1], 50, 100)
+kmin = 1
+kmax = 150
+kstep = 2
+lmin = 1
+lmax = 150
+lstep = 2
+returns = np.zeros((len(range(kmin, kmax, kstep)), len(range(lmin, lmax, lstep))))
 
-g.Run()
-g.Plot()
+dat = DataClass('AAPL', [2012, 10, 1], [2018, 1, 1])
+for k in range(kmin, kmax, kstep):
+    for l in range(lmin, lmax, lstep):
+
+        g = StrategyClass('MovingAverageCrossover', k, l)
+        g.Run(dat)
+
+        returns[int((k - kmin)/kstep), int((l - lmin)/lstep)] = g.value[-1]
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+K, L = np.meshgrid(range(kmin, kmax, kstep), range(lmin, lmax, lstep))
+ax.plot_surface(K, L, returns, cmap = 'plasma')
+plt.show()
